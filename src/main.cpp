e510cd7692084a45ebe97362173b8823b6ac8ed0
by2c4555 <apisitputtasri@gmail.com>
2026-06-24 12:36:39 +0700

    v2.4.3_debug

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,12 @@
 //Adafruit_MAX31865 thermo = Adafruit_MAX31865(10, 11, 12, 13);
 // use hardware SPI, just pass in the CS pin
 Adafruit_MAX31865 thermo = Adafruit_MAX31865(10);
+// The value of the RTD, in ohms, read from the MAX31865
+uint16_t rtd;
+// The ratio of the RTD resistance to the reference resistor
+float ratio;
+
+
 
 // Create a dimmerLamp object
 #define DIMMER_PIN 3
@@ -45,15 +51,6 @@ void setup()
   Serial.println("PID Control of PT100 with MAX31865 and Dimmer");
   thermo.begin(MAX31865_3WIRE);  // set to 2WIRE or 4WIRE as necessary  
   delay(1000);
-  uint16_t rtd = thermo.readRTD();
-  Serial.print("RTD value: "); Serial.println(rtd);
-  float ratio = rtd;
-  ratio /= 32768;
-  Serial.print("Ratio = "); Serial.println(ratio,8);
-  Serial.print("Resistance = "); Serial.println(RREF*ratio,8);
-  Serial.print("Temperature = "); Serial.println(thermo.temperature(RNOMINAL, RREF));
-
-
   Serial.println("MAX31865 3-Wire PT100 RTD Sensor --Initialized");  
   lcd.begin(20, 4);       //     lcd.begin(); for Arduino IDE        
   lcd.backlight();
@@ -82,6 +79,12 @@ void loop()
 uint8_t fault = thermo.readFault();
 if (fault) {
     Serial.print("Fault 0x"); Serial.println(fault, HEX);
+    rtd = thermo.readRTD();
+    Serial.print("RTD value: "); Serial.println(rtd);
+    ratio = rtd;
+    ratio /= 32768;
+    Serial.print("Ratio = "); Serial.println(ratio,8);
+    Serial.print("Resistance = "); Serial.println(RREF*ratio,8);
     Serial.print("Temperature Input : ");
     Serial.println(Input);
     Serial.print("PID Compute output : ");
