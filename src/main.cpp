be7e4ca3c321432212955372de0314d39b0eb1b3
by2c4555 <apisitputtasri@gmail.com>
2026-06-24 11:56:32 +0700

    PT100

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,10 @@
 
 
 // The value of the Rref resistor. Use 430.0 for PT100 and 4300.0 for PT1000
-#define RREF      4300.0
+#define RREF      430.0
 // The 'nominal' 0-degrees-C resistance of the sensor
 // 100.0 for PT100, 1000.0 for PT1000
-#define RNOMINAL  1000.0
+#define RNOMINAL  100.0
 // Use software SPI: CS, DI, DO, CLK
 //Adafruit_MAX31865 thermo = Adafruit_MAX31865(10, 11, 12, 13);
 // use hardware SPI, just pass in the CS pin
